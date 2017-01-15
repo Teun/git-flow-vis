6d617eab2854afcf23ada4c2fdd19383cd8c6b6c
Remie Bolte <r.bolte@gmail.com>
2017-01-15 22:38:26 +0100

    Adding bundled version to the output using Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,14 @@ const uglify = require('gulp-uglify');
 const injectfile = require("gulp-inject-file");
 const del = require('del');
 const vinylPaths = require('vinyl-paths');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
 // ------------------------------------------------------------------------------------------ Tasks
 
 gulp.task('dist', ['build']);
-gulp.task('build', ['standalone', 'commonjs']);
+gulp.task('build', ['standalone', 'commonjs', 'bundle']);
 
 // ------------------------------------------------------------------------------------------ Task Definitions
 
@@ -43,6 +46,23 @@ gulp.task('commonjs', () =>
 		.pipe(rename({
 			basename: 'gitflow-visualize',
 			suffix: '.node'
+		}))
+		.pipe(gulp.dest('dist'))
+)
+
+gulp.task('bundle', ['commonjs'], () => 
+	browserify({ entries: ['dist/gitflow-visualize.node.js'] })
+		.bundle()
+		.pipe(source('gitflow-visualize.bundle.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(buffer())
+		.pipe(uglify({
+			dead_code: true,
+			drop_debugger: true,
+			drop_console: true
 		}))
 		.pipe(gulp.dest('dist'))
 )
