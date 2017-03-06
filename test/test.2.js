e78f0029c11623988b742b50ccba6bc8890a645e
Teun Duynstee <teun@duynstee.com>
2017-03-06 18:41:04 +0100

    #32 basic implementation, should be workable for testing

--- test/test.2.js
+++ test/test.2.js
@@ -32,18 +32,44 @@ suite('Data set 0', function () {
     var data;
     suiteSetup(function(done) {
         var dataCallback = function(d) { d(Dummy2.Data[0]); };
+        var moreCallback = function (sha, d) {
+            if (sha === "b71a7a23456f22f7390d547a8a28782530d191367") {
+                d({values:[{ "id": "b71a7a23456f22f7390d547a8a28782530d191367", "displayId": "b71a7a2", "author": { "name": "teun", "emailAddress": "teun@funda.nl", "id": 301, "displayName": "Teun Duynstee", "active": true, "slug": "teun"}, "authorTimestamp": 1405529398000, "message": "Yet another commit on release zone branch", "parents": [{ "id": "a71a7a23456f22f7390d547a8a28782530d191367", "displayId": "a71a7a2" }] }]}, sha);
+            } else {
+                d(null);
+            }
+        }
+
+        var firstClean = true;
         var dataClean = function(d) {
             data = d;
-            done();
+            if(firstClean){
+                done();
+                firstClean = false;
+            }
         };
-        GitFlowVisualize.draw(null, { dataCallback: dataCallback, dataProcessed: dataClean, releaseTagPattern: /refs\/tags\/(r|h)\d$/, showSpinner: function () { } });
+        GitFlowVisualize.draw(null, { dataCallback: dataCallback, moreDataCallback: moreCallback, dataProcessed: dataClean, releaseTagPattern: /refs\/tags\/(r|h)\d$/, showSpinner: function () { } });
     });
     test('Unrecognized branch should be in release zone', function() {
         var commit = data.commits["a71a7a23456f22f7390d547a8a28782530d191367"];
         var column = data.columns[commit.columns[0]];
 
-        assert(column.name[0] === 'r', "unrecognized branch shoul remain in release zone, because release branch is direct ancestor");
+        assert(column.name[0] === 'r', "unrecognized branch should remain in release zone, because release branch is direct ancestor");
 
+    });
+    test('Adding a new head commit should move everything down a line', function(done) {
+        var commit = data.commits["a71a7a23456f22f7390d547a8a28782530d191367"];
+
+        assert(commit.orderNr === 0, "latest comit must have orderNr 0");
+
+        GitFlowVisualize.branches.setChanged({  "values": [{ "id": "refs/heads/any-branch-that-doesnt-match", "displayId": "any-branch-that-doesnt-match", "latestChangeset": "b71a7a23456f22f7390d547a8a28782530d191367", "isDefault": false }]} );
+        setTimeout(function() {
+            commit = data.commits["a71a7a23456f22f7390d547a8a28782530d191367"];
+            assert(commit.orderNr === 1, "latest commit must have orderNr 1");
+            done();
+        }, 200);
+
+        assert(commit.orderNr === 0, "latest comit must have orderNr 0");
     });
 
 });
